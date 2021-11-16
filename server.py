09edef424893d7a07d759bfd33ea374fe7a2f358
Tomer <tomertzob@gmail.com>
2021-11-16 17:28:42 +0200

    demonstration for server recommend

--- server.py
+++ server.py
@@ -1,95 +1,64 @@
-import numpy as np
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
-import matplotlib.pyplot as plt
+from flask import Flask
+app = Flask(__name__)
+
+#DB on server, only for demonstration
+nikeToAdidas = [{'S':100,'M':50,'L':20},{'S':0,'M':800,'L':200},{'S':1,'M':5,'L':2}]
+
+#if the brand is 'S' and the Typs is 'Adidas' should return 64
+@app.route('localhost/<brand>/<productType>')
+def recommend(brand,productType):
+    Brand = str(brand)
+    Type = str(productType)
+    countSize = [0 for i in range(5)]
+    if (Brand == "S" and Type == "Adidas"):
+        number = 0
+        dict = nikeToAdidas[location(Brand)] #should be replace with DB lists
+        for key, value in dict.items():
+            countSize[location(key)] += value
+        # algorithm
+        totalCount = int(sum(countSize))
+        for i in range(5):
+            sizeNum = (i + 1) * 20
+            number += sizeNum * (countSize[i] / totalCount)
+        return number #retun 64 if success
+    return 0 #return 0 if fail
 
 
-def estimate_coef(x, y):
-    # number of observations/points
-    n = np.size(x)
+def translateSize(size):
+    if (size == 'XS'):
+        return 20
+    if (size == 'S'):
+        return 40
+    if (size == 'M'):
+        return 60
+    if (size == 'L'):
+        return 80
+    if (size == 'Xl'):
+        return 100
 
-    # mean of x and y vector
-    m_x = np.mean(x)
-    m_y = np.mean(y)
-
-    # calculating cross-deviation and deviation about x
-    SS_xy = np.sum(y * x) - n * m_y * m_x
-    SS_xx = np.sum(x * x) - n * m_x * m_x
-
-    # calculating regression coefficients
-    b_1 = SS_xy / SS_xx
-    b_0 = m_y - b_1 * m_x
-
-    return (b_0, b_1)
+def location(size):
+    return int((translateSize(size))/20)-1
 
 
-def plot_regression_line(x, y, b):
-    # plotting the actual points as scatter plot
-    plt.scatter(x, y, color="m",
-                marker="o", s=30)
-
-    # predicted response vector
-    y_pred = b[0] + b[1] * x
-
-    # plotting the regression line
-    plt.plot(x, y_pred, color="g")
-
-    # putting labels
-    plt.xlabel('x')
-    plt.ylabel('y')
-
-    # function to show plot
-    plt.show()
-
-# observations / data
-x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
-
-# estimating coefficients
-b = estimate_coef(x, y)
-print("Estimated coefficients:\nb_0 = {}  \
-\nb_1 = {}".format(b[0], b[1]))
-
-# plotting regression line
-plot_regression_line(x, y, b)
-print("test")
-
-
-# # Load the diabetes dataset
-# diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+# Brand = str('M')
+# Type = str('Adidas')
+# countSize = [0 for i in range(5)]
+# if (Brand == 'M' and Type == 'Adidas'):
+#     number = 0
+#     dict = nikeToAdidas[1]
+#     for key,value in dict.items():
+#         countSize[location(key)] += value
+#     #algorithem
+#     totalCount = int(sum(countSize))
+#     for i in range(5):
+#         sizeNum = (i+1)*20
+#         number += sizeNum * ( countSize[i] / totalCount)
 #
-# # Use only one feature
-# diabetes_X = diabetes_X[:, np.newaxis, 2]
-#
-# # Split the data into training/testing sets
-# diabetes_X_train = diabetes_X[:-20]
-# diabetes_X_test = diabetes_X[-20:]
-#
-# # Split the targets into training/testing sets
-# diabetes_y_train = diabetes_y[:-20]
-# diabetes_y_test = diabetes_y[-20:]
-#
-# # Create linear regression object
-# regr = linear_model.LinearRegression()
-#
-# # Train the model using the training sets
-# regr.fit(diabetes_X_train, diabetes_y_train)
-#
-# # Make predictions using the testing set
-# diabetes_y_pred = regr.predict(diabetes_X_test)
-#
-# # The coefficients
-# print("Coefficients: \n", regr.coef_)
-# # The mean squared error
-# print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
-# # The coefficient of determination: 1 is perfect prediction
-# print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
-#
-# # Plot outputs
-# plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
-# plt.plot(diabetes_X_test, diabetes_y_pred, color="blue", linewidth=3)
-#
-# plt.xticks(())
-# plt.yticks(())
-#
-# plt.show()
+# print(number)
+
+# if __name__ =="__main__":
+#     app.run(debug = True)
+
+# @app.route('/')
+# def index():
+#     print ("hello world")
